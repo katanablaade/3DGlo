8996b53f27646a266dc22ec8be262a56c35f924b
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-11 07:11:18 +0500

    Выполнил усложненные задания lesson20

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,7 @@ const menu = () => {
   const menu = document.querySelector('menu');
   const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
+  const smoothBtn = document.querySelector('a[href="#service-block"');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
@@ -10,8 +11,39 @@ const menu = () => {
 
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
-  menuItems.forEach((menuItem) =>
-    menuItem.addEventListener('click', handleMenu)
-  );
+
+  smoothBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = smoothBtn.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'start',
+      });
+    }
+  });
+
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const targetId = menuItem.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'start',
+        });
+      }
+
+      handleMenu();
+    });
+  });
 };
+
 export default menu;
