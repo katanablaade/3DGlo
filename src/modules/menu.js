b40ed2afaf0a60fc2af971f3e5ae65b075149269
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-15 03:39:15 +0500

    Выполнил дополнительное задание lesson22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,35 +3,43 @@ const menu = () => {
   const menuItems = menu.querySelectorAll('ul>li>a');
   const smoothBtn = document.querySelector('a[href="#service-block"]');
   const main = document.querySelector('main');
+  const body = document.querySelector('body');
 
-  main.addEventListener('click', (e) => {
-    if (e.target.closest('.menu')) {
-      handleMenu();
-    } else if (e.target.closest('a[href="#service-block"')) {
-      e.preventDefault();
-      const targetId = smoothBtn.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      smoothScroll(targetElement);
-    }
-  });
+  const toggleMenu = () => {
+    body.addEventListener('click', (e) => {
+      if (e.target.closest('.menu')) {
+        handleMenu();
+        return;
+      }
+      if (e.target.closest('a[href="#service-block"')) {
+        e.preventDefault();
+        const targetId = smoothBtn.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        smoothScroll(targetElement);
+      }
 
-  menu.addEventListener('click', (e) => {
-    if (e.target.closest('.close-btn')) {
-      handleMenu();
-    } else if (e.target.closest('ul>li>a')) {
-      e.preventDefault();
-      menuItems.forEach((menuItems) => {
-        const menuItem = e.target.closest('ul>li>a');
-        if (menuItems === menuItem) {
-          const targetId = menuItem.getAttribute('href');
-          const targetElement = document.querySelector(targetId);
+      if (e.target.closest('.close-btn')) {
+        handleMenu();
+        return;
+      }
+      if (!e.target.closest('menu')) {
+        menu.classList.remove('active-menu');
+      }
+      if (e.target.closest('ul>li>a')) {
+        e.preventDefault();
+        menuItems.forEach((menuItems) => {
+          const menuItem = e.target.closest('ul>li>a');
+          if (menuItems === menuItem) {
+            const targetId = menuItem.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
 
-          smoothScroll(targetElement);
-          handleMenu();
-        }
-      });
-    }
-  });
+            smoothScroll(targetElement);
+            handleMenu();
+          }
+        });
+      }
+    });
+  };
 
   const smoothScroll = (element) => {
     if (element) {
@@ -46,6 +54,8 @@ const menu = () => {
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
+
+  toggleMenu();
 };
 
 export default menu;
