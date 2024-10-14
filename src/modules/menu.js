0f57e2263154f41627be9dc5143d6417f5763b2a
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-15 02:49:15 +0500

    Выполнил lesson22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,17 +1,41 @@
 const menu = () => {
-  const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
-  const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
-  const smoothBtn = document.querySelector('a[href="#service-block"');
+  const smoothBtn = document.querySelector('a[href="#service-block"]');
+  const main = document.querySelector('main');
 
-  const handleMenu = () => {
-    menu.classList.toggle('active-menu');
-  };
+  main.addEventListener('click', (e) => {
+    if (e.target.closest('.menu')) {
+      handleMenu();
+    } else if (e.target.closest('a[href="#service-block"')) {
+      e.preventDefault();
+      const targetId = smoothBtn.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      smoothScroll(targetElement);
+    }
+  });
+
+  menu.addEventListener('click', (e) => {
+    if (e.target.closest('.close-btn')) {
+      handleMenu();
+    } else if (e.target.closest('ul>li>a')) {
+      e.preventDefault();
+      menuItems.forEach((menuItems) => {
+        const menuItem = e.target.closest('ul>li>a');
+        if (menuItems === menuItem) {
+          const targetId = menuItem.getAttribute('href');
+          const targetElement = document.querySelector(targetId);
+
+          smoothScroll(targetElement);
+          handleMenu();
+        }
+      });
+    }
+  });
 
   const smoothScroll = (element) => {
     if (element) {
-      targetElement.scrollIntoView({
+      element.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
         inline: 'start',
@@ -19,28 +43,9 @@ const menu = () => {
     }
   };
 
-  menuBtn.addEventListener('click', handleMenu);
-  closeBtn.addEventListener('click', handleMenu);
-
-  smoothBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetId = smoothBtn.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-
-    smoothScroll(targetElement);
-  });
-
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const targetId = menuItem.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-
-      smoothScroll(targetElement);
-      handleMenu();
-    });
-  });
+  const handleMenu = () => {
+    menu.classList.toggle('active-menu');
+  };
 };
 
 export default menu;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
-  const closeBtn = modal.querySelector('.popup-close');
 
   let count = 0;
   let idInterval;
@@ -39,12 +38,17 @@ const modal = () => {
     });
   });
 
-  closeBtn.addEventListener('click', () => {
-    if (document.documentElement.clientWidth > 768) {
-      count = 1;
-      requestAnimationFrame(animationModalClose);
-    } else {
-      modal.style.display = 'none';
+  modal.addEventListener('click', (e) => {
+    if (
+      !e.target.closest('.popup-content') ||
+      e.target.classList.contains('popup-close')
+    ) {
+      if (document.documentElement.clientWidth > 768) {
+        count = 1;
+        requestAnimationFrame(animationModalClose);
+      } else {
+        modal.style.display = 'none';
+      }
     }
   });
 };
