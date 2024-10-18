7215b85237bb370f0bc8861bf52870b30067aac0
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-19 00:15:26 +0500

    Выполнил lesson25

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,40 +1,22 @@
+import { animate } from './helpers';
+
 const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
 
-  let count = 0;
-  let idInterval;
-
-  const animationModalOpen = () => {
-    count += 0.05;
-    idInterval = requestAnimationFrame(animationModalOpen);
-
-    if (count < 1) {
-      modal.style.opacity = count;
-    } else {
-      cancelAnimationFrame(idInterval);
-    }
-  };
-
-  const animationModalClose = () => {
-    count -= 0.05;
-    modal.style.opacity = count;
-
-    if (count > 0) {
-      idInterval = requestAnimationFrame(animationModalClose);
-    } else {
-      modal.style.display = 'none';
-      cancelAnimationFrame(idInterval);
-    }
-  };
-
   buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
       modal.style.display = 'block';
-      count = 0;
-      if (document.documentElement.clientWidth > 768) {
-        requestAnimationFrame(animationModalOpen);
-      }
+
+      animate({
+        duration: 200,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+          modal.style.opacity = progress;
+        },
+      });
     });
   });
 
@@ -43,12 +25,18 @@ const modal = () => {
       !e.target.closest('.popup-content') ||
       e.target.classList.contains('popup-close')
     ) {
-      if (document.documentElement.clientWidth > 768) {
-        count = 1;
-        requestAnimationFrame(animationModalClose);
-      } else {
-        modal.style.display = 'none';
-      }
+      animate({
+        duration: 200,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+          modal.style.opacity = 1 - progress;
+        },
+        onComplete: () => {
+          modal.style.display = 'none';
+        },
+      });
     }
   });
 };
